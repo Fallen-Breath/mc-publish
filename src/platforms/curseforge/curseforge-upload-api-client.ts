f161000e2048c61c01f7b900230b96a6d3056641
Kir_Antipov <kp.antipov@gmail.com>
2023-05-30 14:50:11 +0300

    Added temporary logging to investigate #69

--- src/platforms/curseforge/curseforge-upload-api-client.ts
+++ src/platforms/curseforge/curseforge-upload-api-client.ts
@@ -3,6 +3,7 @@ import { MINECRAFT_VERSION_PROVIDER } from "@/games/minecraft";
 import { retry } from "@/utils/async-utils";
 import { isHttpError } from "@/utils/errors";
 import { JavaVersion } from "@/utils/java";
+import { getDefaultLogger } from "@/utils/logging";
 import { Fetch, HttpRequest, createFetch, simpleCache, throwOnError } from "@/utils/net";
 import { CurseForgeError, getInvalidProjectSlug, isCurseForgeError, isInvalidGameVersionIdCurseForgeError, isInvalidProjectSlugCurseForgeError } from "./curseforge-error";
 import { CurseForgeFile, CurseForgeFileInit } from "./curseforge-file";
@@ -259,6 +260,10 @@ async function tryHandleUploadError(error: Error, file: CurseForgeFileInit): Pro
     }
 
     if (isInvalidGameVersionIdCurseForgeError(errorObject)) {
+        // _ TODO: Add proper debug logging here.
+        const logger = getDefaultLogger();
+        logger.warn(`W.I.P. CurseForge rejected these game versions: [${file?.game_versions?.join(", ")}]. ${error}`);
+
         return handleInvalidGameVersionIdCurseForgeError(file);
     }
 
